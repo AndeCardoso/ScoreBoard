f770a8297c921246ff66e19e27d53cf988059f67
Yuri <ypdmarch@gmail.com>
2022-02-01 23:19:49 -0300

    feat: CsvFileManager added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,19 @@
 
-import entities.Game;
-import services.FileManager;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import services.CsvFileManager;
 
 public class App {
     public static void main(String[] args) {
-        FileManager file = new FileManager();
-        ArrayList<Game> list = file.readAllLines();
-        Collections.sort(list, (cur, nex) -> {
-            if (cur.getDateTime() == null || nex.getDateTime() == null) {
-                return 0;
-            }
-            return cur.getDateTime().compareTo(nex.getDateTime());
-        });
-        System.out.println("after" + list);
+//        FileManager file = new FileManager();
+//        ArrayList<Game> list = file.readAllLines();
+//        Collections.sort(list, (cur, nex) -> {
+//            if (cur.getDateTime() == null || nex.getDateTime() == null) {
+//                return 0;
+//            }
+//            return cur.getDateTime().compareTo(nex.getDateTime());
+//        });
+//        System.out.println("after" + list);
+
+        CsvFileManager.readFile();
+        CsvFileManager.convertCsvToList();
     }
 }
