4131b67e7a26ca5346e9fd53b152163fb886e5a2
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-02-01 23:46:20 -0300

    mergering

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,22 @@
-
-import entities.Game;
 import entities.Team;
-import services.FileManager;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.SortedSet;
+import services.CsvFileManager;
 
 public class App {
     public static void main(String[] args) {
-        Team team1 = new Team("Santos");
-        team1.getData();
+//        FileManager file = new FileManager();
+//        ArrayList<Game> list = file.readAllLines();
+//        Collections.sort(list, (cur, nex) -> {
+//            if (cur.getDateTime() == null || nex.getDateTime() == null) {
+//                return 0;
+//            }
+//            return cur.getDateTime().compareTo(nex.getDateTime());
+//        });
+//        System.out.println("after" + list);
+
+        CsvFileManager.readFile();
+        CsvFileManager.convertCsvToList();
+
+//        Team team1 = new Team("Santos");
+//        team1.getData();
     }
 }
