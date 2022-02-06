49436f126445b52efe0c55df19f1dbc7497fb3ff
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-02-06 11:16:02 -0300

    feat: trying make create classification file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,6 @@ public class App {
             FileCreator.createFile(team.getName());
             FileCreator.writeInFiles(team.getName());
         }
-        FileManager.getChampionshipList();
+        FileCreator.createClassification();
     }
 }
--- src/main/java/services/FileCreator.java
+++ src/main/java/services/FileCreator.java
@@ -3,12 +3,40 @@ package services;
 import entities.Team;
 
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 
 public class FileCreator {
 
     static final String OUTPUT_FILE_PATH = "src/resources/output/";
     static final String TEAM_FILE_HEADER = "File containing the matches for ";
+    static final String CLASSIFICATION_FILE_HEADER = "time_1(mandante);time2(visitante);placar_time_1;placar_time_2;data";
+
+    public static void createClassification() {
+        try {
+            String PATH = OUTPUT_FILE_PATH + "final_classification" + ".csv";
+            File finalClassificationFile = new File(PATH);
+            FileWriter myWriter = new FileWriter(PATH);
+            if (finalClassificationFile.createNewFile()) {
+                List<String> list = FileManager.getChampionshipList();
+                myWriter.write(CLASSIFICATION_FILE_HEADER);
+                list.forEach(line -> {
+                    try {
+                        myWriter.write(line);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                myWriter.close();
+                System.out.println("File created: Final Classification");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("A error occurred when creating the file.");
+            e.printStackTrace();
+        }
+    }
 
     public static void createFile(String teamName) {
         try {
