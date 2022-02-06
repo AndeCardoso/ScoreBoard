529234fa33a73815b008ce6c91ece33737032134
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-02-06 11:38:26 -0300

    refac: final file line format fix

--- src/main/java/services/FileCreator.java
+++ src/main/java/services/FileCreator.java
@@ -2,16 +2,18 @@ package services;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileCreator {
 
     private static final String OUTPUT_FILE_PATH = "src/resources/output/";
     private static final String TEAM_FILE_HEADER = "File containing the matches for ";
-    private static final String HEADER_FINAL_FILE = "Team, Victories, Ties, Losses, Points";
+    private static final String HEADER_FINAL_FILE = "Team;Victories;Ties;Losses;Points";
 
     public static void createTeamFile(String teamName) {
         try {
             File teamFile = new File(OUTPUT_FILE_PATH + teamName + ".txt");
+            FileWriter myWriter = new FileWriter(OUTPUT_FILE_PATH + teamName + ".txt");
             if (teamFile.createNewFile()) {
                 System.out.println("File created: " + teamFile.getName());
             } else {
@@ -38,10 +40,18 @@ public class FileCreator {
 
     public static void createFinalFile()  {
         try {
+            List<String> list = FileManager.getChampionshipList();
             FileWriter myWriter = new FileWriter(OUTPUT_FILE_PATH + "FinalFile.csv");
             myWriter.write(HEADER_FINAL_FILE);
             myWriter.write("\n");
-            myWriter.write(String.valueOf(FileManager.getChampionshipList()));
+            list.forEach(line -> {
+                try {
+                    myWriter.write(line);
+                    myWriter.write("\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
             myWriter.close();
             System.out.println("Successfully wrote to the file: " + OUTPUT_FILE_PATH + "FinalFile.csv");
         }catch (IOException e) {
