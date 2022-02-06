e17d592617ac884d30a0e292b52e574a5786d45f
Yuri <ypdmarch@gmail.com>
2022-02-06 14:04:02 +0100

    refac: teamGames set as private

--- src/main/java/entities/Team.java
+++ src/main/java/entities/Team.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Team {
 
     private final String name;
-    public List<Game> teamGames = new ArrayList<Game>();
+    private List<Game> teamGames = new ArrayList<Game>();
     private int victory;
     private int ties;
     private int defeat;
--- src/main/java/services/FileCreator.java
+++ src/main/java/services/FileCreator.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class FileCreator {
 
     static final String OUTPUT_FILE_PATH = "src/resources/output/";
+    static final String TEAM_FILE_HEADER = "File containing the matches for ";
 
     public static void createFile(String teamName) {
         try {
@@ -26,7 +27,8 @@ public class FileCreator {
     public static void writeInFiles(String teamName) {
         try {
             FileWriter myWriter = new FileWriter(OUTPUT_FILE_PATH + teamName + ".txt");
-            myWriter.write(String.valueOf(FileManager.createTeams().get(teamName).teamGames));
+            myWriter.write(TEAM_FILE_HEADER + teamName + ".\n\n");
+            myWriter.write(String.valueOf(FileManager.createTeams().get(teamName).getTeamGames()));
             myWriter.close();
             System.out.println("Successfully wrote to the file: " + OUTPUT_FILE_PATH + teamName + ".txt.");
         } catch (IOException e) {
